5c47d425aa7023ac09a739769d767cae7d33ec19
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-04-17 21:30:21 -0500

    fetching the genres using react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import theme from "./theme.ts";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
--- src/services/useData.ts
+++ src/services/useData.ts
@@ -2,7 +2,7 @@ import { AxiosRequestConfig, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "./api-client";
 
-interface FetchResponse<T> {
+export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
