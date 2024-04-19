8df59748a4746a8997e6161192db2320e2204471
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-04-19 00:44:57 -0500

    Fixed the bug about: initial data in useQueries

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,4 @@
 import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
 import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Genre>("/genres");
@@ -15,7 +14,7 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24h
-    initialData: { count: genres.length, results: genres },
+    // initialData: { count: genres.length, results: genres },
   });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,4 @@
 import { useQuery } from "@tanstack/react-query";
-import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
@@ -15,7 +14,7 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24h
-    initialData: { count: platforms.length, results: platforms },
+    // initialData: { count: platforms.length, results: platforms },
   });
 
 export default usePlatforms;
